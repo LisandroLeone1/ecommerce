b631c267307cc0be4223f2a3b45876c36f084c13
Lisandro Leone <leonelisandro@gmail.com>
2025-03-21 13:23:14 -0300

    Agrego seccionen ingles en el readme

--- config/settings.py
+++ config/settings.py
@@ -41,14 +41,11 @@ INSTALLED_APPS = [
     'ecommerce',
     'carro',
     'login',
-    'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -77,11 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5178",  # URL del frontend en Vite
-]
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
